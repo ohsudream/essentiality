0468a0b0c082fe1cf79f5dd24b0203ea91371d01
Gregory Propf <gpropf@gmail.com>
2014-08-19 20:51:38 -0700

    Renamed my script to be more descriptive

--- training_data_stats.R
+++ training_data_stats.R
@@ -1,19 +1,13 @@
 #!/usr/bin/env R
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 setwd("~")
 setwd("Desktop/DREAM/essentiality") # FIXME - need to read this from a config file
 
 oldNames=as.character(system("ls DREAM/*.gct",intern=TRUE))
 trim <- function(filename) {substr(filename,7,nchar(filename))}
-=======
-oldNames=as.character(system("ls *gct",intern=TRUE))
-trim <- function(filename) {substr(filename,0,nchar(filename))}
->>>>>>> 2b9ae6c1c0aa692f44e6bcef8f85e37b71a37045
-=======
-oldNames=as.character(system("ls *gct",intern=TRUE))
-trim <- function(filename) {substr(filename,0,nchar(filename))}
->>>>>>> 2b9ae6c1c0aa692f44e6bcef8f85e37b71a37045
+
+
+
 newNames <- as.character (lapply(oldNames,trim))
 pairedNames <- data.frame(oldNames,newNames)
 for (i in 1:nrow(pairedNames)) {
@@ -22,8 +16,6 @@ for (i in 1:nrow(pairedNames)) {
                         sep="\t", header=TRUE, skip=2))
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 sds <- lapply(Achilles_v2.9_training.gct[1:14557,3:47],sd)
 df<-data.frame(list(Name=NA,Description=NA),sds)
 row.names(df) <- "Standard Dev"
@@ -42,7 +34,3 @@ medians <- apply(Achilles_v2.9_training.gct[3:47],1,median)
 Achilles_v2.9_training.gct.sds.by.gene <- cbind(Achilles_v2.9_training.gct.sds.by.gene,means,medians)
 Achilles_v2.9_training.gct.sds.by.gene.sorted <- 
   Achilles_v2.9_training.gct.sds.by.gene[order(Achilles_v2.9_training.gct.sds.by.gene$sds),]
-=======
->>>>>>> 2b9ae6c1c0aa692f44e6bcef8f85e37b71a37045
-=======
->>>>>>> 2b9ae6c1c0aa692f44e6bcef8f85e37b71a37045
